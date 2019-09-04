a965b75339b7106f6669156288f4624aabcd00ca
Easton <es686918@ohio.edu>
2019-09-04 16:52:37 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Easton Scowden
+ *        Date: 09/04/2019
+ * Description: This is a program that displays "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
